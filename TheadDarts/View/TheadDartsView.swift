dd1ba86105ebb061d1ffd6b6ce204055b54650b7
Thomas Headley <thomasheadleyjr@gmail.com>
2020-07-05 14:48:52 -0400

    SwiftUI App

---
+++ TheadDarts/View/TheadDartsView.swift
@@ -0,0 +1,18 @@
+//
+//  TheadDartsView.swift
+//  TheadDarts
+//
+//  Created by Thomas Headley on 7/5/20.
+//  Copyright © 2020 Thead. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct TheadDartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
